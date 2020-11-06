0a0e61f025833099aa211637cb53265c1d3f6b43
Salkatholl <73332889+Salkatholl@users.noreply.github.com>
2020-11-06 09:45:59 +0100

    Level 1 - smiley

--- functions - DrawHappyFace-kopi/sketch.js
+++ functions - DrawHappyFace-kopi/sketch.js
@@ -2,29 +2,29 @@ var x, y;
 var radius;
 
 function setup() {
-  createCanvas(600, 200);
-	ellipseMode(CENTER);
+  createCanvas(600, 400);
+  ellipseMode(CENTER);
   noStroke();
 
   x = 300;
-  y = 100;
-  radius = 40;
+  y = 200;
+  radius = 100;
   
 }
 
 function draw() {
-    background(230);
-		if(mouseIsOverEllipse()){
-			fill(0);
-		}
-		else{
-			fill(255);
-		}
-		ellipse(x, y, radius, radius);
+    background(212, 166, 243 );
+    if(mouseIsOver()){
+      drawHappyFace(x,y,true);
+    }
+    else{
+      drawHappyFace(x,y,false);
+    }
+    
 }
 
-function mouseIsOverEllipse(){
-	var result;
+function mouseIsOver(){
+  var result;
   var d = dist(mouseX, mouseY, x, y);
   if (d < radius) {
     result = true;
@@ -32,5 +32,31 @@ function mouseIsOverEllipse(){
     result = false;
   }
   return result;
+}
+
+function drawHappyFace(x, y, blink){
+  push();
+  translate(x,y);
+  //Face
+  fill(249,205,173);//rosy beige
+  ellipse(0, 0, radius, radius);
+
+ //Eye 1
+ fill(30);//dark gray
+ ellipse(0, 10, 10, 10);
+
+ //Eye 2
+ if(blink){
+   rect(10, 10, 10, 2);
+ }
+ else{
+   ellipse(20, 10, 10, 10);
+ }
+ 
+
+ //Mouth
+ fill(252,157,154);//light pink
+ arc(0, 25, 30, 30, 0, radians(180), PIE); 
+ pop();
 }
 
